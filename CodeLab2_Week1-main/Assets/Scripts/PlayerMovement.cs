e95f402ad2e3b678f042c8d70856e6b23881e122
headlesscrown <mdh8533@nyu.edu>
2022-09-03 15:30:39 -0400

    new input system

--- CodeLab2_Week1-main/Assets/Scripts/PlayerMovement.cs
+++ CodeLab2_Week1-main/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 10;
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("W"))
+        {
+            pos.y += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("A"))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("S"))
+        {
+            pos.y -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("D"))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+        transform.position = pos;
     }
 }
