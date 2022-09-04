af879a26e002ba46c0d6a4c8ad786e88d80f32d4
headlesscrown <mdh8533@nyu.edu>
2022-09-03 20:51:44 -0400

    wave setup

--- CodeLab2_Week1-main/Assets/Scripts/CircleSpawn.cs
+++ CodeLab2_Week1-main/Assets/Scripts/CircleSpawn.cs
@@ -12,11 +12,18 @@ public class CircleSpawn : MonoBehaviour
     private float[] xPositions; //float array, predefined positions
 
     [SerializeField]
-    //private GameObject[] circlePrefabs;
-    private Sprite[] sprites;
+    private GameObject[] circlePrefabs;
+    //private Sprite[] sprites;
 
     [SerializeField]
     private Wave[] wave;
+
+    private float currentTime; //calculate time passed since wave started
+
+    List<float> remainingPositions = new List<float>();
+    private int waveIndex; //wave selector
+    float xPos = 0; //position @ which circle will spawn
+    int rand;
     #endregion
 
     //public Sprite[] sprites;
@@ -26,6 +33,9 @@ public class CircleSpawn : MonoBehaviour
     {
         //Instantiate(Resources.Load("Prefabs/Circle"));
         InvokeRepeating("Spawn", 1, 1);
+
+        currentTime = 0; //spawn @ game start
+        remainingPositions.AddRange(xPositions); //add all positions to remainingPos list
     }
 
     // Update is called once per frame
@@ -34,11 +44,31 @@ public class CircleSpawn : MonoBehaviour
         
     }
 
-    void Spawn()
+    //void Spawn()
+    //{
+    //    GameObject go = Instantiate(Resources.Load("Prefabs/Circle")) as GameObject; //spawn prefabs
+    //    int num = GetComponent<ColorPicker>().SetSprite(); //call colour picker
+    //    go.GetComponent<SpriteRenderer>().sprite = sprites[num]; //call sprite renderer
+    //}
+
+    void SpawnCircle(float xPos)
     {
-        GameObject go = Instantiate(Resources.Load("Prefabs/Circle")) as GameObject; //spawn prefabs
-        int num = GetComponent<ColorPicker>().SetSprite(); //call colour picker
-        go.GetComponent<SpriteRenderer>().sprite = sprites[num]; //call sprite renderer
+        int r = Random.Range(0, 4); //4 circle types
+        GameObject circleObj = Instantiate(circlePrefabs[r], new Vector3(xPos, transform.position.y, 0), Quaternion.identity); //spawn @ same pos as spawner; quaternion.identity = cancel rotation, use circleObj as default
+
+    }
+
+    void SelectWave()
+    {
+        remainingPositions = new List<float>(); //reset list
+        remainingPositions.AddRange(xPositions); //add positions
+
+        waveIndex = Random.Range(0, wave.Length); //random select
+
+        currentTime = wave[waveIndex].delayTime;
+
+        if (wave[waveIndex].spawnAmount == 1)
+            xPos = Random.Range(-xLimit, xLimit);
     }
 }
 
