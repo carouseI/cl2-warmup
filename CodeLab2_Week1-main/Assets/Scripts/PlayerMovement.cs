78fca130489550956579b86ad4e84e4710d4c05a
headlesscrown <mdh8533@nyu.edu>
2022-09-04 12:48:45 -0400

    temp save

--- CodeLab2_Week1-main/Assets/Scripts/PlayerMovement.cs
+++ CodeLab2_Week1-main/Assets/Scripts/PlayerMovement.cs
@@ -5,22 +5,28 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
-    float speed;
+    float movementSpeed;
 
     private Vector3 currentPosition;
 
     // Update is called once per frame
     void Update()
     {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(horizontalInput * movementSpeed * Time.deltaTime, verticalInput * movementSpeed * Time.deltaTime, 0);
+
         Movement(); //call
     }
 
     public void Movement()
     {
+
         //if (Input.GetKey(KeyCode.W)) currentPosition.z += speed * Time.deltaTime; //enable vertical movement (u)
         //if (Input.GetKey(KeyCode.S)) currentPosition.z -= speed * Time.deltaTime; //enable vertical movement (d)
-        if (Input.GetKey(KeyCode.D)) currentPosition.x += speed * Time.deltaTime; //enable lateral movement (l)
-        if (Input.GetKey(KeyCode.A)) currentPosition.x -= speed * Time.deltaTime; //enable lateral movement (r)
+        if (Input.GetKey(KeyCode.D)) currentPosition.x += movementSpeed * Time.deltaTime; //enable lateral movement (l)
+        if (Input.GetKey(KeyCode.A)) currentPosition.x -= movementSpeed * Time.deltaTime; //enable lateral movement (r)
 
         transform.position = currentPosition; //set to current position
     }
